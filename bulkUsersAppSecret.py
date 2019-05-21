78a34f9027b8e6d1086093dd30c1ae547f22b945
shau-chi <46990248+shau-chi@users.noreply.github.com>
2019-05-21 10:43:03 -0700

    Add files via upload

--- bulkUsersAppSecret.py
+++ bulkUsersAppSecret.py
@@ -2,7 +2,7 @@
 #Use this version if Require App Secret Proof has been enabled
 #This script requires the access token to be placed in a file named accessToken
 #This script requires the app secret to be placed in a file named appSecret
-import requests, json, hmac, hashlib, time
+import requests, json, hmac, hashlib, time, ast
 
 #Get the access token from a file named accessToken
 f = open("accessToken", "r")
@@ -46,8 +46,18 @@ except:
 
 print("inactive people")
 for person in people:
+    #Get the app secret time and generate the app secret proof
+    t = str(int(time.time()))
+    blurb = TOKEN+'|'+t
+    blurb = bytes(blurb)
+    appsecret_proof = hmac.new(APPSECRET, blurb, hashlib.sha256)
+    appsecret_proof = appsecret_proof.hexdigest()
     member = "https://graph.workplace.com/%s/feed?access_token=%s&appsecret_proof=%s&appsecret_time=%s" % (person['id'], TOKEN, appsecret_proof, t)
     r = requests.get(member, headers=headers)
     answer = r.json()
     if answer['data'] == []:
         print("%s, %s") % (person['userName'], person['id'])
+    usage = r.headers['x-app-usage']
+    usage = ast.literal_eval(usage)
+    if usage['call_count'] > 90:
+        time.sleep(60)
